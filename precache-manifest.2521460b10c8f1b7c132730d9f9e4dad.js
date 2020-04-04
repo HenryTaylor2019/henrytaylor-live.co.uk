c908cd5279fd4086ad6f727ee731d51abc98a395
HenryTaylor2019 <henrytaylor@live.co.uk>
2020-04-04 10:30:09 +0100

    Updates

---
+++ precache-manifest.2521460b10c8f1b7c132730d9f9e4dad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa6fee0c6c5939a1e3fa53dc9521a56c",
+    "url": "/pingpong-team-generator/index.html"
+  },
+  {
+    "revision": "4ce01f7a0063589cdc02",
+    "url": "/pingpong-team-generator/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "3282e032b848c3796706",
+    "url": "/pingpong-team-generator/static/js/2.dc2ecf12.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pingpong-team-generator/static/js/2.dc2ecf12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ce01f7a0063589cdc02",
+    "url": "/pingpong-team-generator/static/js/main.8b13d018.chunk.js"
+  },
+  {
+    "revision": "15ee95b0beee138d45f2",
+    "url": "/pingpong-team-generator/static/js/runtime-main.ddabd565.js"
+  }
+]);
